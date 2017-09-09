8cab9e57c01659917b114322af423184a858e678
Joseph <fskreuz@gmail.com>
2017-09-09 16:11:31 -0400

    build script

---
+++ packages/ractive/karma.conf.js
@@ -0,0 +1,19 @@
+const base = require('../../karma.conf.js')
+
+module.exports = function (config) {
+  config.set(Object.assign({}, base, {
+    plugins: base.plugins.concat(['karma-phantomjs-launcher']),
+    browsers: ['PhantomJS'],
+    logLevel: config.LOG_DEBUG,
+    files: [
+      { pattern: '../../node_modules/simulant/dist/simulant.umd.js', nocache: true },
+      { pattern: '../../node_modules/qunit-assert-html/dist/qunit-assert-html.js', nocache: true },
+      { pattern: 'lib.umd.js', nocache: true },
+      { pattern: 'tests.iife.js', nocache: true },
+      { pattern: 'qunit/*.gif', served: true, included: false, watched: false, nocache: false },
+    ],
+    proxies: {
+      '/qunit/': '/base/qunit/'
+    }
+  }))
+}
